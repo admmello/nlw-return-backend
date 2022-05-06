bf14cb7af496adee63e3b6b82d9621db9725adf5
admmello <admmello@gmail.com>
2022-05-06 08:07:01 -0400

    config backend to deploy

---
+++ dist/use-cases/submit-feedback-use-case.spec.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const submit_feedback_use_case_1 = require("./submit-feedback-use-case");
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+const submitFeedback = new submit_feedback_use_case_1.SubmitFeedbackUseCase({ create: createFeedbackSpy }, { sendMail: sendMailSpy });
+describe('Submit feedback', () => {
+    it('should be able to submit a feedback', async () => {
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64flefnkrlh',
+        })).resolves.not.toThrow();
+        expect(createFeedbackSpy).toBeCalled();
+        expect(sendMailSpy).toBeCalled();
+    });
+    it('should not be able to submit a feedback without type', async () => {
+        await expect(submitFeedback.execute({
+            type: '',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64flefnkrlh',
+        })).rejects.toThrow();
+    });
+    it('should not be able to submit a feedback without comment', async () => {
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: '',
+            screenshot: 'data:image/png;base64flefnkrlh',
+        })).rejects.toThrow();
+    });
+    it('should not be able to submit a feedback with an invalid screenshot', async () => {
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'screenshot.png',
+        })).rejects.toThrow();
+    });
+});
